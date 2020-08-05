fd11165ffdc92b2f4414bee5ab00dfeb08433b1a
Kyler Mejia <kylermejia1@gmail.com>
2020-08-05 02:55:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,10 +24,11 @@ end
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.shift
-  
 end
 
 def retrieve_element_from_index(array, index_number)
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   
   
 end
 
